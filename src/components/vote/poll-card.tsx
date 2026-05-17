7dc9f4746ce3ce2ad824b11335ba1c31828a84ba
bimon08 <bimonlangnongsiej@gmail.com>
2026-05-17 17:32:16 +0530

    refactor: remove organization fee display from poll card prize distribution

--- src/components/vote/poll-card.tsx
+++ src/components/vote/poll-card.tsx
@@ -182,15 +182,6 @@ function PrizeBreakdownTooltip({
                                     return (
                                         <>
                                             {prizeRows}
-                                            {distribution.orgFee > 0 && (
-                                                <>
-                                                    <div className="border-t border-white/20 my-1" />
-                                                    <div className="flex items-center justify-between gap-4 text-white/60 text-xs">
-                                                        <span>💼 Org</span>
-                                                        <span>{distribution.orgFee.toLocaleString()} <CurrencyIcon size={12} /></span>
-                                                    </div>
-                                                </>
-                                            )}
                                         </>
                                     );
                                 })()}
